72fa254630f2d64868b718b83a3a57e23849666b
Richi Jeffery <richi1717@gmail.com>
2022-03-21 17:17:22 -0500

    remove console log

--- next.config.js
+++ next.config.js
@@ -1,19 +1,5 @@
 require('dotenv').config()
-console.log({
-  env: {
-    dbItems: `${process.env.DB_URL}/studies`,
-    debug: true,
-    playAudio: false,
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    dbUrl: process.env.DB_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messageSenderId: process.env.MESSAGE_SENDER_ID,
-    appId: process.env.APP_ID,
-    appUrl: process.env.APP_URL,
-  },
-})
+
 module.exports = {
   env: {
     dbItems: `${process.env.DB_URL}/studies.json`,
