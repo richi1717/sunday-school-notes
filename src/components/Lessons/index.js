dfc826de7a0128272b5e8a4d0f6993fa75f65bdc
Richi Jeffery <richi1717@gmail.com>
2022-05-01 00:06:21 -0500

    fix: bad word break css

--- src/components/Lessons/index.js
+++ src/components/Lessons/index.js
@@ -92,7 +92,7 @@ export default function Lessons ({ lessons, isAdmin }) {
                     <Stack direction={{ mobile: 'column', tablet: 'row' }} spacing={2}>
                       <Typography
                         component={MuiMarkdown}
-                        sx={{ 'wordBreak': 'break-all', 'p:first-child': { mt: 0 } }}
+                        sx={{ 'wordBreak': 'break-word', 'p:first-child': { mt: 0 } }}
                         onClick={() => {
                           setLesson(note[1])
                           setUpdateId(note[0])
